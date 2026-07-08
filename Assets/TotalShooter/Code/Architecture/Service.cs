c46f3f3c0d711286bae25daad39dc99b4e7b3116
Gleb Kuzmin <kuzmin@owlcat.games>
2026-07-08 13:40:10 +0200

    Organization

---
+++ Assets/TotalShooter/Code/Architecture/Service.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sadalmalik.TotalShooter.Architecture
+{
+    public static class Service
+    {
+        internal static Dictionary<Type, object> m_Instances = new Dictionary<Type, object>();
+        
+        public static T Get<T>()
+        {
+            return m_Instances.TryGetValue(typeof(T), out object instance) ? (T) instance : default(T);
+        }
+
+        public static void Add<T>() where T : new()
+        {
+            m_Instances.Add(typeof(T), new T());
+        }
+        
+        public static void Add<T>(T instance)
+        {
+            m_Instances.Add(typeof(T), instance);
+        }
+
+        // Type I can be class or interface
+        public static IEnumerable<I> GetAllOf<I>()
+        {
+            foreach (var (type, instance) in m_Instances)
+            {
+                if (type.IsAssignableFrom(typeof(I)))
+                    yield return (I) instance;
+            }
+        }
+    }
+}
